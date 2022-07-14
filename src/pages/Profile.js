0e4b46bdaf39c3a536c133afa3e4d5117ee056d5
Kelvin Nunez <knunezweb@gmail.com>
2022-07-14 11:44:04 -0400

    Save Files

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,6 @@
 import DisplayPreviousUser from "../components/displayPreviousUser";
 
 const Profile = () => {
-
     return (
         <main className="min-h-screen  box-border">
             <h1 className="text-center p-5 text-4xl text-purple-500">Profile Page</h1>
